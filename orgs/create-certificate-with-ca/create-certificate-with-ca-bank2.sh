c1e6ccc7b68abe68287c3f96b7487710b4eb0b65
Dhananjay1 Patil <Dhananjay1.Patil@lntinfotech.com>
2022-08-28 11:17:31 +0530

    peer 0 replaced with respective one

--- orgs/create-certificate-with-ca/create-certificate-with-ca-bank2.sh
+++ orgs/create-certificate-with-ca/create-certificate-with-ca-bank2.sh
@@ -24,10 +24,10 @@ createCertificateForOrg2() {
     OrganizationalUnitIdentifier: orderer' >${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/config.yaml
 
   echo
-  echo "Register peer0"
+  echo "Register peer1"
   echo
 
-  fabric-ca-client register --caname ca.bank1.barclays.com --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client register --caname ca.bank1.barclays.com --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
 #   echo
 #   echo "Register peer1"
@@ -48,36 +48,36 @@ createCertificateForOrg2() {
   fabric-ca-client register --caname ca.bank1.barclays.com --id.name bank2admin --id.secret bank2adminpw --id.type admin --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
   mkdir -p ../crypto-config/peerOrganizations/bank2.barclays.com/peers
-  mkdir -p ../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com
+  mkdir -p ../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com
 
   # --------------------------------------------------------------
   # Peer 0
   echo
-  echo "## Generate the peer0 msp"
+  echo "## Generate the peer1 msp"
   echo
 
-  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:8054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/msp --csr.hosts peer0.bank2.barclays.com --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer1:peer1pw@localhost:8054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/msp --csr.hosts peer1.bank2.barclays.com --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/msp/config.yaml
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/msp/config.yaml
 
   echo
-  echo "## Generate the peer0-tls certificates"
+  echo "## Generate the peer1-tls certificates"
   echo
 
-  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:8054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls --enrollment.profile tls --csr.hosts peer0.bank2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer1:peer1pw@localhost:8054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls --enrollment.profile tls --csr.hosts peer1.bank2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/server.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/keystore/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/server.key
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/ca.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/server.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/keystore/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/server.key
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/tlscacerts/ca.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/msp/tlscacerts/ca.crt
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/tlsca
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/tlsca/tlsca.bank2.barclays.com-cert.pem
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/tlsca/tlsca.bank2.barclays.com-cert.pem
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/ca
-  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer0.bank2.barclays.com/msp/cacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/ca/ca.bank1.barclays.com-cert.pem
+  cp ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/peers/peer1.bank2.barclays.com/msp/cacerts/* ${PWD}/../crypto-config/peerOrganizations/bank2.barclays.com/ca/ca.bank1.barclays.com-cert.pem
 
   # --------------------------------------------------------------------------------
   #  Peer 1
--- orgs/create-certificate-with-ca/create-certificate-with-ca-non-bank.sh
+++ orgs/create-certificate-with-ca/create-certificate-with-ca-non-bank.sh
@@ -23,9 +23,9 @@ createcertificatesForOrg3() {
     OrganizationalUnitIdentifier: orderer' >${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/config.yaml
 
   echo
-  echo "Register peer0"
+  echo "Register peer2"
   echo
-  fabric-ca-client register --caname ca.bank1.barclays.com --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client register --caname ca.bank1.barclays.com --id.name peer2 --id.secret peer2pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
   echo
   echo "Register peer1"
@@ -46,32 +46,32 @@ createcertificatesForOrg3() {
 
   # -----------------------------------------------------------------------------------
   #  Peer 0
-  mkdir -p ../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com
+  mkdir -p ../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com
 
   echo
-  echo "## Generate the peer0 msp"
+  echo "## Generate the peer2 msp"
   echo
-  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:10054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/msp --csr.hosts peer0.nonbank.barclays.com --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:10054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/msp --csr.hosts peer2.nonbank.barclays.com --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/msp/config.yaml
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/msp/config.yaml
 
   echo
-  echo "## Generate the peer0-tls certificates"
+  echo "## Generate the peer2-tls certificates"
   echo
-  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:10054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls --enrollment.profile tls --csr.hosts peer0.nonbank.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://peer2:peer2pw@localhost:10054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls --enrollment.profile tls --csr.hosts peer2.nonbank.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/server.crt
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/keystore/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/server.key
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/ca.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/server.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/keystore/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/server.key
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/tlscacerts/ca.crt
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/msp/tlscacerts/ca.crt
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/tlsca
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/tlsca/tlsca.nonbank.barclays.com-cert.pem
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/tlsca/tlsca.nonbank.barclays.com-cert.pem
 
   mkdir ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/ca
-  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer0.nonbank.barclays.com/msp/cacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/ca/ca.nonbank.barclays.com-cert.pem
+  cp ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/peers/peer2.nonbank.barclays.com/msp/cacerts/* ${PWD}/../crypto-config/peerOrganizations/nonbank.barclays.com/ca/ca.nonbank.barclays.com-cert.pem
 
   # ------------------------------------------------------------------------------------------------
 
