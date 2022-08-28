1e3e827e366a33c88689b2b3cd7deb0db99f30e7
Dhananjay1 Patil <Dhananjay1.Patil@lntinfotech.com>
2022-08-28 16:47:15 +0530

    sushants suggestions implemented

--- orgs/create-certificate-with-ca-orderer/create-certificate-with-ca-orderer.sh
+++ orgs/create-certificate-with-ca-orderer/create-certificate-with-ca-orderer.sh
@@ -6,46 +6,46 @@ createCretificateForOrderer() {
 
   export FABRIC_CA_CLIENT_HOME=${PWD}/../crypto-config/ordererOrganizations/barclays.com
 
-  fabric-ca-client enroll -u https://admin:adminpw@localhost:7054 --caname ca.bank1.barclays.com --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://admin:adminpw@localhost:9054 --caname ca-orderer --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   echo 'NodeOUs:
   Enable: true
   ClientOUIdentifier:
-    Certificate: cacerts/localhost-7054-ca-bank1-barclays-com.pem
+    Certificate: cacerts/localhost-9054-ca-orderer.pem
     OrganizationalUnitIdentifier: client
   PeerOUIdentifier:
-    Certificate: cacerts/localhost-7054-ca-bank1-barclays-com.pem
+    Certificate: cacerts/localhost-9054-ca-orderer.pem
     OrganizationalUnitIdentifier: peer
   AdminOUIdentifier:
-    Certificate: cacerts/localhost-7054-ca-bank1-barclays-com.pem
+    Certificate: cacerts/localhost-9054-ca-orderer.pem
     OrganizationalUnitIdentifier: admin
   OrdererOUIdentifier:
-    Certificate: cacerts/localhost-7054-ca-bank1-barclays-com.pem
+    Certificate: cacerts/localhost-9054-ca-orderer.pem
     OrganizationalUnitIdentifier: orderer' >${PWD}/../crypto-config/ordererOrganizations/barclays.com/msp/config.yaml
 
   echo
   echo "Register orderer"
   echo
 
-  fabric-ca-client register --caname ca.bank1.barclays.com --id.name orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client register --caname ca-orderer --id.name orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   echo
 #   echo "Register orderer2"
 #   echo
 
-#   fabric-ca-client register --caname ca.bank1.barclays.com --id.name orderer2 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client register --caname ca-orderer --id.name orderer2 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   echo
 #   echo "Register orderer3"
 #   echo
 
-#   fabric-ca-client register --caname ca.bank1.barclays.com --id.name orderer3 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client register --caname ca-orderer --id.name orderer3 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   echo
   echo "Register the orderer admin"
   echo
 
-  fabric-ca-client register --caname ca.bank1.barclays.com --id.name ordererAdmin --id.secret ordererAdminpw --id.type admin --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client register --caname ca-orderer --id.name ordererAdmin --id.secret ordererAdminpw --id.type admin --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   mkdir -p ../crypto-config/ordererOrganizations/barclays.com/orderers
   # mkdir -p ../crypto-config/ordererOrganizations/barclays.com/orderers/barclays.com
@@ -59,7 +59,7 @@ createCretificateForOrderer() {
   echo "## Generate the orderer msp"
   echo
 
-  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/msp --csr.hosts orderer.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/msp --csr.hosts orderer.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/msp/config.yaml
 
@@ -67,7 +67,7 @@ createCretificateForOrderer() {
   echo "## Generate the orderer-tls certificates"
   echo
 
-  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls --enrollment.profile tls --csr.hosts orderer.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls --enrollment.profile tls --csr.hosts orderer.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls/ca.crt
   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer.barclays.com/tls/server.crt
@@ -88,7 +88,7 @@ createCretificateForOrderer() {
 #   echo "## Generate the orderer2 msp"
 #   echo
 
-#   fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/msp --csr.hosts orderer2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/msp --csr.hosts orderer2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/msp/config.yaml
 
@@ -96,7 +96,7 @@ createCretificateForOrderer() {
 #   echo "## Generate the orderer2-tls certificates"
 #   echo
 
-#   fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls --enrollment.profile tls --csr.hosts orderer2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls --enrollment.profile tls --csr.hosts orderer2.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls/ca.crt
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer2.barclays.com/tls/server.crt
@@ -113,7 +113,7 @@ createCretificateForOrderer() {
 #   echo "## Generate the orderer3 msp"
 #   echo
 
-#   fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/msp --csr.hosts orderer3.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/msp --csr.hosts orderer3.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/msp/config.yaml
 
@@ -121,7 +121,7 @@ createCretificateForOrderer() {
 #   echo "## Generate the orderer3-tls certificates"
 #   echo
 
-#   fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls --enrollment.profile tls --csr.hosts orderer3.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+#   fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls --enrollment.profile tls --csr.hosts orderer3.barclays.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls/ca.crt
 #   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/barclays.com/orderers/orderer3.barclays.com/tls/server.crt
@@ -138,7 +138,7 @@ createCretificateForOrderer() {
   echo "## Generate the admin msp"
   echo
 
-  fabric-ca-client enroll -u https://ordererAdmin:ordererAdminpw@localhost:7054 --caname ca.bank1.barclays.com -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/users/Admin@barclays.com/msp --tls.certfiles ${PWD}/fabric-ca/bank1/tls-cert.pem
+  fabric-ca-client enroll -u https://ordererAdmin:ordererAdminpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/barclays.com/users/Admin@barclays.com/msp --tls.certfiles ${PWD}/fabric-ca/orderer/tls-cert.pem
 
   cp ${PWD}/../crypto-config/ordererOrganizations/barclays.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/barclays.com/users/Admin@barclays.com/msp/config.yaml
 
